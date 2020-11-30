929049fb3d92ac29bb7fb341bb0d60a37304a168
khardrix <carmenandryanhuffman@yahoo.com>
2020-11-29 19:11:19 -0500

    final build

--- MyInsertionSort.h
+++ MyInsertionSort.h
@@ -31,21 +31,3 @@ void myInsertionSort(RandomAccessIterator beginIter, RandomAccessIterator endIte
 
 #endif //MYSORTINGTESTS_MYINSERTIONSORT_H
 
-/*
- public class InsertionSort {
-   The method for sorting the numbers
-public static void insertionSort(int[] list) {
-    for (int i = 1; i < list.length; i++) {
-         // insert list[i] into a sorted sublist list[0..i-1] so that
-             // list[0..i] is sorted.
-        int currentElement = list[i];
-        int k;
-        for (k = i - 1; k >= 0 && list[k] > currentElement; k--) {
-            list[k + 1] = list[k];
-        }
-
-        // Insert the current element into list[k+1]
-        list[k + 1] = currentElement;
-    }
-}
- */
